3217cffd08d7f85b1451a49ea523c8c809b33ac9
king-d-dev <davidoreal1@gmail.com>
2021-07-08 10:37:21 +0000

    now reading some of the config values from environment

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-const MONGO_URI = 'mongodb://localhost:27017/hola-chat';
-const WEB_URL = 'http://localhost:3000';
+const MONGO_URI = process.env.MONGO_URI!;
+const WEB_URL = process.env.WEB_URL!;
 const AUTH0_AUDIENCE = 'http://hola/api';
 const AUTH0_DOMAIN = 'https://dev-appoh.us.auth0.com';
 
